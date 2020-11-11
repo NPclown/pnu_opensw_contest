cd2626edae64293efabd1fd13ce69f8684c4aad9
이현아 <hyn2626@naver.com>
2020-11-11 21:07:41 +0900

    css수정 최종

---
+++ client/src/components/AddEditor.js
@@ -0,0 +1,49 @@
+import 'codemirror/lib/codemirror.css';
+import '@toast-ui/editor/dist/toastui-editor.css';
+import React ,{useRef} from 'react'
+import { Editor } from '@toast-ui/react-editor';
+import Button from '@material-ui/core/Button';
+
+function AddEditor(props) {
+  const editorRef = React.useRef();
+
+  const handleClick = (index) =>{
+    switch (index){
+        case 0:
+            props.setTitle(editorRef.current.getInstance().getHtml())
+            break
+        case 1:
+            props.setCont(editorRef.current.getInstance().getHtml())
+            console.log(editorRef.current.getInstance().getHtml())
+            break
+        case 2:
+            props.setTestcase(editorRef.current.getInstance().getHtml())
+            break
+        case 3:
+            props.setCode(editorRef.current.getInstance().getHtml())
+            break
+        case 4:
+            props.setInit(editorRef.current.getInstance().getHtml())
+            break
+        case 5:
+            props.setScore(editorRef.current.getInstance().getHtml())
+            break
+        }
+  }
+
+    return (
+      <>
+        <Editor
+          previewStyle="vertical"
+          height="400px"
+          initialEditType="markdown"
+          placeholder= {props.placeholder}
+          ref={editorRef}
+        />
+        <Button onClick={()=>{handleClick(props.value);alert("등록완료")}} variant="contained" color="primary" disableElevation>
+            저장
+        </Button>
+      </>
+    );
+}
+export default AddEditor;
--- client/src/pages/AddProblem.js
+++ client/src/pages/AddProblem.js
@@ -1,29 +1,124 @@
-import React from 'react'
-import Split from 'react-split';
-import { TabContent, TabPane, Nav, NavItem, NavLink, Table, Button } from 'reactstrap';
+import React ,{useState} from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import 'codemirror/lib/codemirror.css';
+import AddEditor from '../components/AddEditor'
+import Axios from 'axios';
 
-function AddProblem(){
-    return(
-        <Split
-        direction="horizontal"
-        sizes={[50, 50]}
-        minSize={0}
-        gutterSize={12}
-        className="split-parent-horizontal"
-        >
-        <div className="problem-pane">
-          문제치시오
-        </div>
-        <Split direction="vertical" sizes={[70, 30]} minSize={[100, 0]} gutterSize={4}>
-        <div>
-            여기는 코드치시오
-        </div>
-        <TabPane>
-            여기는 결과
-        </TabPane>
-        </Split>
-      </Split>
-    )
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
 }
 
-export default AddProblem;
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `vertical-tab-${index}`,
+    'aria-controls': `vertical-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+    display: 'flex',
+    height: '100%',
+  },
+  tabs: {
+    borderRight: `1px solid ${theme.palette.divider}`,
+  },
+}));
+
+export default function AddProblem(props) {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+  const [title,setTitle] =useState("")
+  const [cont,setCont] =useState("")
+  const [testcase,setTestcase] =useState("")
+  const [code,setCode] =useState("")
+  const [init,setInit] =useState("")
+  const [score,setScore] =useState("")
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const toBackendAdd = async(e) => {
+    e.preventDefault()
+    var result2;
+        try{
+            result2 = await Axios.post(`/api/workbooks`,{name:title, 
+              cont:cont,inits:init, code: code, score: score,sample: testcase });
+        } catch(error) {
+            alert(error)
+        }
+    }
+
+  return (
+    <div className={classes.root}>
+      <Tabs
+        orientation="vertical"
+        variant="scrollable"
+        value={value}
+        onChange={handleChange}
+        aria-label="Vertical tabs example"
+        className={classes.tabs}
+      >
+        <Tab label="문제이름" {...a11yProps(0)} />
+        <Tab label="문제내용" {...a11yProps(1)} />
+        <Tab label="테스트케이스" {...a11yProps(2)} />
+        <Tab label="초깃값" {...a11yProps(3)} />
+        <Tab label="코드" {...a11yProps(4)} />
+        <Tab label="점수" {...a11yProps(5)} />
+        <Tab label="제출하기" onClick ={(e)=>{toBackendAdd(e);alert("제출완료")}}{...a11yProps(6)} />
+      </Tabs>
+      <TabPanel value={value} index={0}>
+        <AddEditor value={value} setTitle={setTitle} title={title} placeholder= '문제제목을 작성해 주세요.'></AddEditor>
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <AddEditor value={value} setCont={setCont} placeholder= '문제를 작성해 주세요.'></AddEditor>
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        <AddEditor value={value} setTestcase={setTestcase} placeholder= '테스트케이스를 작성해 주세요.'></AddEditor>
+      </TabPanel>
+      <TabPanel value={value} index={3}>
+        <AddEditor value={value} setCode={setCode} placeholder= '채점코드를 작성해 주세요.'></AddEditor>
+      </TabPanel>
+      <TabPanel value={value} index={4}>
+        <AddEditor value={value} setInit={setInit} placeholder= '초기코드를 작성해 주세요.'></AddEditor>
+      </TabPanel>
+      <TabPanel value={value} index={5}>
+        <AddEditor value={value} setScore={setScore} placeholder= '점수를 작성해 주세요.'></AddEditor>
+      </TabPanel>
+      <TabPanel index={6}>
+        제출이 완료되었습니다.
+      </TabPanel>
+    </div>
+  );
+}
