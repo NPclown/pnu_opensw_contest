fdf5f4cfc40d139d70785d425ef956fffce64772
donghyun son <nigrumspiritus@gmail.com>
2020-11-11 16:08:23 +0900

    소스코드 재분배

--- client/src/components/Workspace.js
+++ client/src/components/Workspace.js
@@ -1,4 +1,4 @@
-import React ,{useState} from 'react';
+import React ,{useEffect, useState} from 'react';
 import Split from 'react-split';
 import { TabContent, TabPane, Nav, NavItem, NavLink, Table, Button } from 'reactstrap';
 import Editor from './Editor';
@@ -6,6 +6,9 @@ import './Workspace.css';
 import AceContext from '../context/AceContext';
 
 function Workspace(props) {
+  const [result,setResult] = useState({data:{}, isLoading:true})
+  const [resultT,setResultT] = useState({data:{}, isLoading:true})
+
   const state = {
     input: '',
     result: null,
@@ -15,78 +18,65 @@ function Workspace(props) {
     aceProps: {
       theme: 'dawn',
       keyboardHandler: ''
-    }}
+  }}
 
-    return (
-       <AceContext.Provider value={state.aceProps}>
-        <Split direction="vertical" sizes={[50, 50]} minSize={[100, 0]} gutterSize={4}>
-          <Editor
-            testcase={props.testcase}
-            setTestCase={props.setTestCase}
-            show={props.show}
-            handleClose={props.handleClose} 
-            handleShow={props.handleShow}
-            setShow={props.setShow}
-            sample={props.sample}
-            setLanguage={props.setLanguage}
-            id={props.id}
-            toBackEnd={props.toBackEnd}
-            toBackEnd2={props.toBackEnd2}
-            inits={props.inits} language={props.language} 
-            setGo={props.setGo} go={props.go} code={props.code} setCode={(value)=>props.setCode(value)}
-            setResult={props.setResult} setResultT={props.setResultT}
-          />
-          <div className="results-view">
-            { Object.keys(props.result.data).length === 0?(
-                 ""
-                ):(<div>
-                  {console.log(props.result)}
-                  점수: {props.result.data.score}
-                  {props.result.data.result.map((problemresult, index)=>{
-                  return(
-                    <div>
-                      <div className="top-line">                
-                      결과: {problemresult.success?("성공"):("실패")}
-                      </div>
-                      <div>                
-                      런타임: {problemresult.time}
-                      </div>
+  return (
+      <AceContext.Provider value={state.aceProps}>
+      <Split direction="vertical" sizes={[50, 50]} minSize={[100, 0]} gutterSize={4}>
+        <Editor
+          {...props}
+          setResult={setResult} setResultT={setResultT}
+        />
+        <div className="results-view">
+          { Object.keys(result.data).length === 0?(
+                ""
+              ):(<div>
+                {console.log(result)}
+                점수: {result.data.score}
+                {result.data.result.map((problemresult, index)=>{
+                return(
+                  <div>
+                    <div className="top-line">                
+                    결과: {problemresult.success?("성공"):("실패")}
                     </div>
-                    )})}
-                </div>)}
-                {Object.keys(props.resultT.data).length === 0?(
-                  ""
-                ):(<div>
-                  <div className="p-2">
-                    {props.resultT.data.result.map((problemresult, index)=>{
-                        return(
-                          <div>
-                            <div className="top-line">                
-                            <p><b>결과:</b>{problemresult.success?("성공"):("실패")}</p> 
-                            </div>
-                            <div>                
-                            <p><b>입력값:</b> {problemresult.input}</p>
-                            </div>
-                            <div>                
-                            <p><b>기댓값:</b> {problemresult.output}</p>
-                            </div>
-                            <div>                
-                            <p><b>실행결과:</b> {problemresult.result}</p>
-                            </div>
-                            <div>                
-                            {problemresult.prints}
-                            </div>
-                          </div>
-                        )
-                    })}
+                    <div>                
+                    런타임: {problemresult.time}
+                    </div>
                   </div>
-                )
-              </div>
-              )}
-          </div>
-        </Split>
-       </AceContext.Provider>
-    );
-  }
+                  )})}
+              </div>)}
+              {Object.keys(resultT.data).length === 0?(
+                ""
+              ):(<div>
+                <div className="p-2">
+                  {resultT.data.result.map((problemresult, index)=>{
+                      return(
+                        <div>
+                          <div className="top-line">                
+                          <p><b>결과:</b>{problemresult.success?("성공"):("실패")}</p> 
+                          </div>
+                          <div>                
+                          <p><b>입력값:</b> {problemresult.input}</p>
+                          </div>
+                          <div>                
+                          <p><b>기댓값:</b> {problemresult.output}</p>
+                          </div>
+                          <div>                
+                          <p><b>실행결과:</b> {problemresult.result}</p>
+                          </div>
+                          <div>                
+                          {problemresult.prints}
+                          </div>
+                        </div>
+                      )
+                  })}
+                </div>
+            </div>
+            )}
+        </div>
+      </Split>
+      </AceContext.Provider>
+  );
+}
 
 export default Workspace;
--- client/src/pages/WorkspacePage.js
+++ client/src/pages/WorkspacePage.js
@@ -6,94 +6,41 @@ import './WorkspacePage.css';
 import Workspace from '../components/Workspace';
 import Axios from 'axios';
 
-  function WorkspacePage(props){
-    const [code,setCode] = useState("")
-    const [language,setLanguage] = useState({name:"c",value:1})
-    const [id,setId] = useState("1")
-    const [show, setShow] = useState(false);
-    const [go,setGo] = useState(true)
-    const [testcase,setTestCase] = useState([])
-    const [result,setResult] = useState({data:{}, isLoading:true})
-    const [resultT,setResultT] = useState({data:{}, isLoading:true})
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-    const [state,setState]=useState({data:{}, isLoading:true})
+function WorkspacePage(props){
+  const [state,setState]=useState({data:{}, isLoading:true})
 
-    useEffect(() => {
-      const getData = async() => {
-          try{
-              var result = await Axios.get(`/api/workbooks/${props.match.params.id}`);
-              setState({data : result.data.data.items, isLoading:false})
-              setCode(result.data.data.items.inits[language.name])
-          } catch(error) {
-              alert(error)
-              setState({data : {}, isLoading:true})
-          }
-      }
-      getData();
-  },[id,language])
+  useEffect(() => {
+    const getData = async() => {
+        try{
+            var result = await Axios.get(`/api/workbooks/${props.match.params.id}`);
+            setState({data : result.data.data.items, isLoading:false})
+        } catch(error) {
+            alert(error)
+            setState({data : {}, isLoading:true})
+        }
+    }
+    getData();
+  },[props.match.params.id])
 
-const toBackEnd = async(e,id,language,code,testcase) => {
-  e.preventDefault()
-  var result3;
-      try{
-          result3 = await Axios.post(`/run/execution`,{id : id, 
-              language:language.value, code:code, testcase:testcase});
-          setResultT({data: result3.data.data.items, isLoading:false})
-
-      } catch(error) {
-          alert(error)
-      }
-  }
-  
-  const toBackEnd2 = async(e,id,language,code) => {
-      e.preventDefault()
-      var result2;
-          try{
-              result2 = await Axios.post(`/run/score`,{id : id, 
-                  language:language.value, code:code});
-              setResult({data: result2.data.data.items,isLoading:false})
-          } catch(error) {
-              alert(error)
-          }
-      }
   return state.isLoading ? (
     <div className="d-flex h-100 align-items-center justify-content-center">
-    <Spinner style={{ width: '3rem', height: '3rem' }} type="grow" />
-  </div>
-    )  :
-    (
-      <Split
-        direction="horizontal"
-        sizes={[50, 50]}
-        minSize={0}
-        gutterSize={12}
-        className="split-parent-horizontal"
-      >
-        <div className="problem-pane">
-          <Problem cont={state.data.cont} />
-        </div>
-        <Workspace
-          handleClose={handleClose} 
-          handleShow={handleShow}
-          testcase={testcase}
-          setTestCase={setTestCase}
-          show={show}
-          setShow={setShow}
-          sample={state.data.sample}
-          id={id}
-          toBackEnd={toBackEnd}
-          toBackEnd2={toBackEnd2}
-          result={result}
-          resultT={resultT}
-          language={language} setLanguage={setLanguage}
-          inits={state.data.inits[language.name]}
-          setGo={setGo} go={go} code={code} setCode={(value)=>setCode(value)}
-          setResult={setResult} setResultT={setResultT}
-          />
-      </Split>
-    )
-  }
+      <Spinner style={{ width: '3rem', height: '3rem' }} type="grow" />
+    </div>
+    ) : (
+    <Split
+    direction="horizontal"
+    sizes={[50, 50]}
+    minSize={0}
+    gutterSize={12}
+    className="split-parent-horizontal"
+    >
+      <div className="problem-pane">
+        <Problem cont={state.data.cont} />
+      </div>
+      <Workspace id = {props.match.params.id} data = {state.data}/>
+    </Split>
+  )
+}
 
 export default WorkspacePage;
 
